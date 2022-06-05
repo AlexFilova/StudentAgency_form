eed7332845a5bd233da32414b3a4ba2d2ad7a745
Alexandra Filova <filova.alex@gmail.com>
2022-06-05 19:31:33 +0200

    json-server + get data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Footer from './components/mainComponents/footer/Footer';
 import './App.css';
 
 const App = () => {
-console.log('render');
+
+// console.log('render');
+
   return (
       <div>
         <Header/>
--- src/components/mainComponents/body/subform/SubformPreferency.js
+++ src/components/mainComponents/body/subform/SubformPreferency.js
@@ -1,5 +1,6 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import { SubFormTitleFont, SmallTitleFont } from "../../../../styles/fontStyles";
+import {fetchData} from '../../../../api/apiCall';
 import { useTranslation } from "react-i18next";
 import Select from '../../../commonComponents/selectAndInput/Select';
 import { SubFormContainer } from './style';
@@ -9,11 +10,47 @@ import ErrorMessage from '../../../commonComponents/ErrorMessage';
 
 const SubformPreferency = () => {
 
-    console.log('render Sunform');
     const { t } = useTranslation();
 
     const [designToggleBtnOne, setdesignToggleBtnOne] = useState(true);
     const [designToggleBtnTwo, setdesignToggleBtnTwo] = useState(false);
+
+    const [optionsEuCountries, setOptionsEuCountries] = useState('');
+    const [optionsUsaStates, setOptionsUsaStates] = useState('');
+    const [optionsJobs, setOptionsJobs] = useState('');
+
+    
+    useEffect(() => {
+        const getData = async () => {
+        const data = await fetchData();
+        const euCountries = data[0];
+        const arrayEuCountries = Object.values(euCountries);
+        const optionsAttributesEuCountries =
+            arrayEuCountries[0].map(euCountry => ({
+                value: euCountry,
+                label: euCountry,
+            }))
+            setOptionsEuCountries(optionsAttributesEuCountries);
+        const usaStates = data[1];
+        const arrayUsaStates = Object.values(usaStates);
+        const optionsAttributesUsaStates =
+            arrayUsaStates[0].map(usaState => ({
+                value: usaState,
+                label: usaState,
+            }))
+            setOptionsUsaStates(optionsAttributesUsaStates);
+        const jobs = data[2];
+        const arrayJobs = Object.values(jobs);
+        const optionsAttributesJobs =
+            arrayJobs[0].map(job => ({
+                value: job,
+                label: job,
+            }))
+            setOptionsJobs(optionsAttributesJobs);
+        };
+        getData()
+      },[]);
+      
     
     return ( 
         <SubFormContainer>
@@ -32,11 +69,7 @@ const SubformPreferency = () => {
                      id={'country'}
                      selectLabel={t('internship.labelCountry')}
                      placeholder={t('internship.palceholderCountry')}
-                     // options={posts.shelters.map((item: any) => ({
-                     //     option: item.id,
-                     //     label: item.name
-                     //     }))
-                     // }
+                     options={optionsEuCountries}
                      />
                     :
                     <Select
@@ -44,11 +77,7 @@ const SubformPreferency = () => {
                     id={'state'}
                     selectLabel={t('internship.labelState')}
                     placeholder={t('internship.palceholderState')}
-                    // options={posts.shelters.map((item: any) => ({
-                    //     option: item.id,
-                    //     label: item.name
-                    //     }))
-                    // }
+                    options={optionsUsaStates}
                     />
                 }
                 <Select
@@ -56,6 +85,7 @@ const SubformPreferency = () => {
                     id={'work'}
                     selectLabel={t('internship.labelWorkArea')}
                     placeholder={t('internship.palceholderWorkArea')}
+                    options={optionsJobs}
                     />
                 <ErrorMessage>{t('errorMessages.missingCoutry')}</ErrorMessage>
             </div>
