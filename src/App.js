9425f9b5abbd10347082af15fba45b5cce71b097
Alexandra Filova <filova.alex@gmail.com>
2022-07-05 00:05:29 +0200

    className was removed from App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 import Header from './components/mainComponents/header/Header';
 import Main from './components/mainComponents/main/Main';
 import Footer from './components/mainComponents/footer/Footer';
-import './App.css';
 
 const App = () => {
 
   return (
-      <div className={App}>
+      <div>
         <Header/>
         <Main />
         <Footer/>
